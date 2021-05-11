0c3d7f7f3d4d3d6127271315e5f4a53596fa62a8
Chris-Ack <chris_ackermann@web.de>
2021-05-12 07:58:31 +0900

    Trying to fix gitignore

--- server/api.js
+++ server/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("../knexfile");
 const path = require("path");
 const cors = require("cors");
+const stripe = require("stripe")("pk_test_51IpqSaAvO61I5XZRj77hWRG7epQ8fnU632M5wF2tr305UFYMpjqFwiIxmnjOdPD1cyicOWb99iYdEUzyvE5g8Con00cg4nIZOX")
 
 const app = express();
 
@@ -37,6 +38,21 @@ app.post("/api/tasukete/delete", async (req, res) => {
   await db("tasukete").where({ id: inputId }).del();
   return res.sendStatus(204);
 });
+
+/////////////////////////STRIPE ENDPOINT///////////////////////////
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd"
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+///////////////////////////////////////////////////////////////////
+
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
