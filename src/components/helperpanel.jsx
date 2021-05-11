2098b0470ef60511e073f8e4b116dfed60a41e7c
GaetanKarst <gaetan.karst@gmail.com>
2021-05-11 21:31:37 +0900

    scafold the basic info panel

---
+++ src/components/helperpanel.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+export default function HelperPanel() {
+    //TODO fetch information from database: image, name, location, id, dob, description, {services: price}
+
+    const [information, setInformation] = useState([])
+    const [orderInfo, setOrderInfo] = useState([])
+
+    const getHelper = async() => {
+        //TODO check the route
+        const res = await axios.get('/api/helper/:name');
+        setInformation(res.data);
+    }
+
+    const getOrderInfo = async() => {
+        //TODO check the route for service database
+        const res = await axios.get('/api/orderinfo/:orderid');
+        setOrderInfo(res.data);
+    }
+
+    useEffect(() => {
+        getHelper();
+        getOrderInfo();
+    }, []);
+
+
+    return (
+        <>
+            <div className="helper-panel">
+
+                <img src="../src/asset/owl.jpeg" alt="helper-photo"/>
+                <div className="helper-information">
+                    <h1>{information.name} is ready to help you! 🎃</h1>
+                    <h2>description</h2>
+                    {information.description}
+                    <h2>Service</h2>
+                    <ul>
+                        {/* TODO check what info database holds */}
+                        <li>{orderInfo}</li>
+                    </ul>
+                </div>
+
+            </div>
+        </>
+    )
+}
