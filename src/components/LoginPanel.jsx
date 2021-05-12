daef621c7bbee8fa0261622e882a5e003dc7e65f
Chris-Ack <chris_ackermann@web.de>
2021-05-12 20:50:35 +0900

    router logic working, including login redirect.

--- src/components/LoginPanel.jsx
+++ src/components/LoginPanel.jsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react';
-
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 export default function LoginPanel ({users, currentUser, setCurrentUser, username, setUsername, password, setPassword}) {
 
     const [userField, setUserField] = useState("")
     const [passField, setPassField] = useState("")
+    const [passedCheck, setPassedCheck] = useState(false)
 
     async function loginSubmitButton() {
         const correctUser = users.filter(user => user.username === username && user.password === password)
         console.log(correctUser)
         if (correctUser.length === 0) {console.log("WRONG"); return alert("Error, please select matching username and password.") }
         else {
-            setCurrentUser(correctUser);
-        }
+            setCurrentUser(correctUser)
+            setPassedCheck(true)}
     }
-
-    return (
-        <>
+/*
+    useEffect(() => {
+        currentUser.length !== 0;
+        console.log("trigger")
+    }, [currentUser])
+*/
+    return passedCheck ? (
+        <section>
+            <Redirect to="/formpage" />
+        </section>
+    ) : (
+        <section>
             <p>Please input Username and Password</p>
                 <form>                
                         <label htmlFor="userField">Username: </label>
@@ -25,7 +35,6 @@ export default function LoginPanel ({users, currentUser, setCurrentUser, usernam
                         <input type="password" id="passField" name="passField" maxLength="16"onChange={(e) => setPassword(e.target.value)}/><br/>
                 </form>
             <button className="loginSubmitButton" onClick={() => loginSubmitButton()}>Submit</button>
-        </>
-    )
-    
+        </section>
+        )
 }
