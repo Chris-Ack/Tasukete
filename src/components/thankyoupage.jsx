2824ef1c631a802e28d6b11e5872458ca2e250bb
Evan Trujillo <evomatic@pop-os.localdomain>
2021-05-12 20:54:01 +0900

    added thank you component

---
+++ src/components/thankyoupage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+export default function ThankYou() {
+    return (
+    <>
+         <div className='thankyou-container'>
+           <h1 className='thankyou'>Thank you!</h1>
+           <p className="email"><strong>Please check your email</strong> for further instructions on how to contact your helper.</p>            
+        </div>
+    </>
+    )
+}
+
+
+
+
+
+
+
