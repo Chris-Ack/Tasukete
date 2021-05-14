e8d33b44c155f7e6bc0fecf4565656480c396e8b
Chris-Ack <chris_ackermann@web.de>
2021-05-14 15:20:31 +0900

    thankyoupage

--- src/components/thankyoupage.jsx
+++ src/components/thankyoupage.jsx
@@ -5,7 +5,7 @@ export default function ThankYou() {
     return (
     <>
         <section>
-         <div className='thankyou-container'>
+         <div className='thankyou-container' >
            <h1 className='thankyou'>Thank you!</h1>
            <p className="email"><strong>Please check your email</strong> for further instructions on how to contact your helper.</p>            
         </div>
