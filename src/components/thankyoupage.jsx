708f2a977b42be137f9f2da75a9b5b6e044c3262
Julie Stocks <julie.stocks@gmail.com>
2021-05-14 15:10:47 +0900

    Fixed the form
    Co-authored-by: Evan Trujillo <Evomatic@users.noreply.github.com>
    Co-authored-by: Chris Ackermann <Chris-Ack@users.noreply.github.com>

--- src/components/thankyoupage.jsx
+++ src/components/thankyoupage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function ThankYou() {
     return (
     <>
-         <div className='thankyou-container'>
+         <div className='helper-panel'>
            <h1 className='thankyou'>Thank you!</h1>
            <p className="email"><strong>Please check your email</strong> for further instructions on how to contact your helper.</p>            
         </div>
